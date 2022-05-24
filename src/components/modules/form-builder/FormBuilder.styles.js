2b45c98e2384621d6ce8de304b8972362313fabd
Kevin Voß <kevin.voss@micromerce.com>
2022-05-24 21:40:48 +0200

    adjust form layout

--- src/components/modules/form-builder/FormBuilder.styles.js
+++ src/components/modules/form-builder/FormBuilder.styles.js
@@ -3,6 +3,12 @@ import styled from "styled-components";
 const FormBuilderContainer = styled.div`
   margin: 1rem;
   border-left: 1px solid black;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
 `;
 
 const StateContainer = styled.div`
